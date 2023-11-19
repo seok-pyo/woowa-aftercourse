a6bbbef3b3097566a70634223c4921f4720d8150
seok-pyo <hongseokpyou@gmail.com>
2023-11-19 13:20:11 +0900

    feat(validation): 입력값 길이 예외 처리

--- javascript-baseball-6/__tests__/ValidationTest.js
+++ javascript-baseball-6/__tests__/ValidationTest.js
@@ -6,4 +6,10 @@ describe('유효성 검사 테스트', () => {
 
     expect(() => validate(input)).toThrow();
   });
+
+  test('입력값의 길이가 3이 안되는 경우 예외를 발생시킨다', () => {
+    const input = '1234';
+
+    expect(() => validate(input)).toThrow();
+  });
 });
--- javascript-baseball-6/src/validation.js
+++ javascript-baseball-6/src/validation.js
@@ -1,6 +1,12 @@
 const validate = {
   type(userInput) {
     const numberCheck = /[^1-9]/;
-    if (numberCheck.test(userInput)) throw new Error('잘못된 입력입니다.');
+    if (numberCheck.test(userInput))
+      throw new Error('[ERROR] 잘못된 입력입니다.');
+  },
+
+  length(userInput) {
+    if (userInput.length < 3 || userInput.length > 3)
+      throw new Error('[ERROR] 잘못된 입력입니다.');
   },
 };
