36268a20adfda4725181bc04242ce293c725a4b5
seok-pyo <hongseokpyou@gmail.com>
2023-11-25 13:01:10 +0900

    feat(car): 거리 기록 기능 구현

--- javascript-racingcar-6/src/Car.js
+++ javascript-racingcar-6/src/Car.js
@@ -2,13 +2,19 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 export default class Car {
   #name;
+  #distance;
+
   constructor(name) {
     this.#name = name;
+    this.#distance = 0;
   }
 
   goForward() {
     // if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) return true;
     // else false;
-    return MissionUtils.Random.pickNumberInRange(0, 9) >= 4;
+    const forwardCheck = MissionUtils.Random.pickNumberInRange(0, 9) >= 4;
+    if (forwardCheck >= 4) this.#distance += 1;
+
+    return forwardCheck >= 4;
   }
 }
