d8c91f0ab76f58ad27b31d2b0c849aa245e76ec1
seok-pyo <hongseokpyou@gmail.com>
2023-12-01 12:17:24 +0900

    feat(input): 입력 기능 구현

--- javascript-lotto-6/src/input.js
+++ javascript-lotto-6/src/input.js
@@ -4,8 +4,21 @@ const input = {
   async money() {
     const money =
       await MissionUtils.Console.readLineAsync('구입금액을 입력해 주세요.');
-    const quantity = money / 1000;
 
     return { money, quantity };
+  },
+
+  async winningNum() {
+    const winningNumber =
+      await MissionUtils.Console.readLineAsync('당첨 번호를 입력해주세요.');
+
+    return winningNumber;
+  },
+
+  async bonusNum() {
+    const bonusNumber =
+      await MissionUtils.Console.readLineAsync('보너스 번호를 입력해 주세요.');
+
+    return bonusNumber;
   },
 };
