b494f3eb772a60d8f3e8c22fa527643fa7443d8d
seok-pyo <hongseokpyou@gmail.com>
2023-12-01 12:33:21 +0900

    feat(output): 출력 기능 구현

---
+++ javascript-lotto-6/src/output.js
@@ -0,0 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const output = {
+  quantity(number) {
+    MissionUtils.Console.print(`${number}개를 구매했습니다.`);
+  },
+
+  number(lotto) {
+    MissionUtils.Console.print(lotto.getNumbers);
+  },
+
+  resultMessage() {
+    MissionUtils.Console.print('당첨 통계\n---');
+  },
+
+  result(message, resultNumber) {
+    MissionUtils.Console.print(
+      `${message.rankNumber}개 일치${message.price} - ${resultNumber}개`,
+    );
+  },
+
+  protif(resultProfit) {
+    MissionUtils.Console.print(`총 수익률은 ${resultProfit}입니다.`);
+  },
+};
