3f2616a816237c506c5bcfd511b7dc64fd0ff8df
hongseokpyo <hongseokpyou@gmail.com>
2023-12-09 16:35:48 +0900

    할인 적용 여부 확인 기능

---
+++ javascript-christmas-6/src/discountCheck.js
@@ -0,0 +1,32 @@
+class Discount {
+  static dDayCheck(day) {
+    const amount = 1000;
+    if (day > 25) return 0;
+    if (day <= 25 && day >= 1) {
+      return amount + day * 100;
+    }
+  }
+  static weekCheck(day) {
+    const isWeekend = true;
+    const dayNum = new Date(2023, 12 - 1, day);
+    if (dayNum < 5) {
+      isWeekend = false;
+      return isWeekend;
+    }
+    if (5 <= dayNum) {
+      return isWeekend;
+    }
+  }
+  static starCheck(day) {
+    const isStar = false;
+    const dayNum = new Date(2023, 12 - 1, day);
+    if (dayNum === 0 || dayNum === 25) {
+      isStar = true;
+      return isStar;
+    }
+
+    return isStar;
+  }
+
+  static giftCheck(totalPrice) {}
+}
